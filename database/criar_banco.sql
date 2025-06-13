a2e8c3694e4ca83ef14f3a916a2663220981b7c6
Renan Ewbank <147399245+renanewbank@users.noreply.github.com>
2025-06-12 22:07:14 -0300

    Update criar_banco.sql

--- database/criar_banco.sql
+++ database/criar_banco.sql
@@ -4,7 +4,7 @@ USE `sistema`;
 DROP TABLE IF EXISTS `usuario`;
 
 CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `senha_hash` varchar(255) NOT NULL,
@@ -17,6 +17,3 @@ INSERT INTO `usuario` (`id`, `nome`, `email`, `senha_hash`, `tipo`) VALUES
 (2, 'renan', 'renan@gmail.com', '$2y$10$Hn1eC5k6To7RQsxgeoJt0eHLWuo24G.zmy5SzHs1Ay/kCiJFmwVcm', 'user'),
 (3, 'teste', 'teste@teste.com', '$2y$10$L99CfrFdjgYWGzIfY2cC2ecfR0kXa92LvBgGZH8cw4.1NPVVetejW', 'user');
 
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
